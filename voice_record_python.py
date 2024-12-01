af80ea6441f196cb652e7c97276fa26eb07bb297
seemaparvinmn <145323108+Seemaparvinmn@users.noreply.github.com>
2024-12-01 11:29:53 +0530

    Update voice_record_python.py

--- voice_record_python.py
+++ voice_record_python.py
@@ -1,20 +1,3 @@
-#
-
-
-# import sounddevice
-# from scipy.io.wavfile import write
-
-
-# def sharadvoice_recorder(seconds,file):
-#     print("Your Recording Started.....")
-#     sharadrecording=sounddevice.rec((seconds*44100),samplerate=44100,channels=2)
-#     sounddevice.wait()
-#     write(file,44100,sharadrecording)
-#     print(" Recordin is finished")
-
-
-# sharadvoice_recorder(10," Recordsave.wav")
-
 
 
 import sounddevice as sd
